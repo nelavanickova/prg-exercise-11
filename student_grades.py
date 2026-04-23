248a5499ca2cac7dc29973f15b1187b0f8de4517
nelavanickova <nela.1515@seznam.cz>
2026-04-23 15:59:08 +0200

    kontrola1

---
+++ student_grades.py
@@ -0,0 +1,18 @@
+class StudentsGrades:
+    def __init__(self, scores):
+        self.scores = scores
+
+    def get_by_index(self, index):
+        return self.scores[index]
+
+    def count(self):
+        return len(self.scores)
+
+
+
+if __name__ == "__main__":
+    results = StudentsGrades([85, 42, 91, 67, 50, 73, 100, 38, 58])
+
+    print(results.count())  # 9
+    print(results.get_by_index(2))  # 91
+    print(results.scores)  # [85, 42, 91, 67, 50, 73, 100, 38, 58]
